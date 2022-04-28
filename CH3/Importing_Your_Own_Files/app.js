592473b2230d823a6b2b13e2b343c2725138baa9
tamer_maarouf <tamermaarouf@gmail.com>
2022-04-29 00:13:23 +0200

    install sharp chalk lib

--- CH3/Importing_Your_Own_Files/app.js
+++ CH3/Importing_Your_Own_Files/app.js
@@ -1,6 +1,7 @@
 
 import validator from 'validator';
 import utils from './utils.js';
+import chalk from 'chalk';
 // import add from './utils.js';
 
 
@@ -8,5 +9,8 @@ const sum = utils.add(4, 4);
 
 console.log(utils.name);
 console.log(sum);
+console.log(chalk.redBright.inverse.bold('Tamer Maarouf in redBright text'));
+console.log(chalk.green('Success'));
 console.log(validator.isEmail('tamermaarouf@gmail.com'));
+
 
